bae22fb7c55855315030af3fb7bbaa2257e0c163
Shalini1908 <singhparmar1908@gmail.com>
2023-01-18 21:29:02 +0530

    added day-2

--- wish-kart/src/Pages/Home.jsx
+++ wish-kart/src/Pages/Home.jsx
@@ -5,6 +5,9 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
+      <div style={{marginTop:"200px"}}>
+        <img src="https://images.meesho.com/images/marketing/1631722939962.webp" alt="b" style={{margin:"auto"}}  />
+      </div>
 </div>
   )
 }
