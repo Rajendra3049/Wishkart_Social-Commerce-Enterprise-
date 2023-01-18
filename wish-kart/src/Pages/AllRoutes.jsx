abfc174b39e07126caf4ff63e3575cc512d21093
Rajendra3049 <Rajendrapatel3049@gmail.com>
2023-01-18 15:23:27 +0530

    fix all routes conflict

--- wish-kart/src/Pages/AllRoutes.jsx
+++ wish-kart/src/Pages/AllRoutes.jsx
@@ -3,20 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import { Otp } from "../components/signup/otp";
 import Admin from "./admin";
 import SignUp from "./signup";
+import Products from "./Products";
 export default function AllRoutes() {
   return (
     <Routes>
       <Route path="/signup" element={<SignUp />} />
       <Route path="/otp" element={<Otp />} />
       <Route path="/admin" element={<Admin />} />
+      <Route path="/products" element={<Products />} />
     </Routes>
   );
-
-import Products from "./Products";
-
-export default function AllRoutes() {
-  return <Routes>
-    <Route path="/products" element={<Products/>}></Route>
-  </Routes>;
-
 }
