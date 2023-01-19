4361ec03af1c6625036af4287c1922b33f9e0ee8
Md Jawed <mdjawedh22@gmail.com>
2023-01-19 21:59:49 +0530

    code update

--- wish-kart/src/components/signup/signup.jsx
+++
@@ -1,3 +0,0 @@
-// export default function SignUp() {
-//   return <Routes></Routes>;
-// }
--- wish-kart/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-// import {
-//   legacy_createStore,
-//   compose,
-//   combineReducers,
-//   applyMiddleware,
-// } from "redux";
-// import thunk from "redux-thunk";
-
-// // const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
