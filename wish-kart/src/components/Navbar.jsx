61ee2e737394b9a449a96c6f74ea8ab57880642c
Rajendra3049 <Rajendrapatel3049@gmail.com>
2023-01-20 12:52:55 +0530

    main no changes

--- wish-kart/src/components/Navbar.jsx
+++ wish-kart/src/components/Navbar.jsx
@@ -35,31 +35,39 @@ import { BiShoppingBag } from "react-icons/bi";
 export default function Navbar() {
   // const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-  <>
-  <Box top={0} w={"100%"} h={"55px"} zIndex={1} position={"fixed"}>
-  <Box
-  width={["1300px", "1300px", "100%"]}
-  justifyContent={"center"}
-  h={["158x"]}
-  bg={"white"}
-  border={"1px solid gray"}
-  position={"relative"}>
-  <Flex h={"100px"} alignItems={"center"} w={"100%"}>
-  <Link href="/" w={["9%", "9%", "8%"]}
-  h={["99px", "99px", "100px"]}
-  ml={["20px", "20px", "20px"]}
-  mt={["-26px", "", ""]}>
-  <Image src={logo}/>
-  </Link>
-  <Flex>
-  <Stack spacing={3}
-   w={["200px", "300px", "400px"]}
-   ml={["5%", "5%", "10%"]}
-   mt={"-20px"}>
-   <Input placeholder="Try Saree,Kurti or Search by Product Code" width={["80%", "80%", "100%"]} h={["25px", "30px", "45px"]}
-   color={"black"}
-  borderWidth="1px"
-  fontSize={["8px", "8x", "15px"]} p={7}/>
+    <>
+      <Box top={0} w={"100%"} h={"55px"} zIndex={1} position={"fixed"}>
+        <Box
+          width={["1300px", "1300px", "100%"]}
+          justifyContent={"center"}
+          h={["158x"]}
+          bg={"white"}
+          border={"1px solid gray"}
+          position={"relative"}>
+          <Flex h={"100px"} alignItems={"center"} w={"100%"}>
+            <Link
+              href="/"
+              w={["9%", "9%", "8%"]}
+              h={["99px", "99px", "100px"]}
+              ml={["20px", "20px", "20px"]}
+              mt={["-26px", "", ""]}>
+              <Image src={logo} />
+            </Link>
+            <Flex>
+              <Stack
+                spacing={3}
+                w={["200px", "300px", "400px"]}
+                ml={["5%", "5%", "10%"]}
+                mt={"-20px"}>
+                <Input
+                  placeholder="Try Saree,Kurti or Search by Product Code"
+                  width={["80%", "80%", "100%"]}
+                  h={["25px", "30px", "45px"]}
+                  color={"black"}
+                  borderWidth="1px"
+                  fontSize={["8px", "8x", "15px"]}
+                  p={7}
+                />
               </Stack>
             </Flex>
 
@@ -187,5 +195,3 @@ export default function Navbar() {
     </>
   );
 }
-
-
